804b04a4459d795c2aab2bbf14302ad2df07ad88
Owen Daughtery <OwenJ.daughtery@gmail.com>
2019-04-28 17:32:42 +0100

    persons prefab and script

---
+++ FamilyTreeMaker/Assets/Scripts/Gender.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gender : ScriptableObject
+{
+    enum gender {Male, Female};
+}
---
+++ FamilyTreeMaker/Assets/Scripts/Person.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class Person : MonoBehaviour
+{
+    [SerializeField]
+    Person mother;
+    [SerializeField]
+    Person father;
+    [SerializeField]
+    List<Person> siblings = new List<Person>();
+    [SerializeField]
+    Person partner;
+    [SerializeField]
+    List<Person> children = new List<Person>();
+
+    public string firstName;
+    public string lastName;
+    public Gender gender;
+    [SerializeField]
+    public DateTime dob;
+
+    private Texture2D box;
+
+    public void Start()
+    {
+
+    }
+
+}
